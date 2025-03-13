9e13f4d6af41dfaedef105962e74d35069ee4ea0
WhiteNoise01 <153906600+SEFWHITE-Dev@users.noreply.github.com>
2025-03-13 16:46:54 +0900

    add procfile and webconfig

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/e-commerce-web-app.jar
---
+++ src/main/java/com/sefwhitedev/ecom_project/WebConfig.java
@@ -0,0 +1,14 @@
+package com.sefwhitedev.ecom_project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://e-commerce-web-app-production-11b7.up.railway.app");
+    }
+}
